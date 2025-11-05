be6c0a2368f8005c5268f6353081f985d4148494
Youmna Ahmed <ahmed232@CAMPUS@V304V-NCP040.campus.unicaen.fr>
2025-11-05 08:53:05 +0100

    Modification site et controller

--- site.php
+++ site.php
@@ -7,7 +7,6 @@ set_include_path("./src");
 
 /* Inclusion des classes utilisées dans ce fichier */
 require_once("Router.php");
-
 /*
  * Cette page est simplement le point d'arrivée de l'internaute
  * sur notre site. On se contente de créer un routeur
