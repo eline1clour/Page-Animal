f07306483f5ec5dd3982670f63c4c2881fa4bfe9
Youmna Ahmed <ahmed232@C304L-127A15.campus.unicaen.fr>
2026-03-12 12:39:59 +0100

    Mise à jour du projet

--- mysql_config.example.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('MYSQL_HOST', 'mysql:host=localhost;');
-define('MYSQL_PORT', 'port=3306;');
-define('MYSQL_DB', 'dbname=animals');
-define('MYSQL_USER', 'username');
-define('MYSQL_PASSWORD', 'password');
-?>
---
+++ mysql_config.php
@@ -0,0 +1,7 @@
+<?php
+define('MYSQL_HOST', 'mysql:host=localhost;');
+define('MYSQL_PORT', 'port=3306;');
+define('MYSQL_DB', 'dbname=nom_de_la_base');
+define('MYSQL_USER', 'nom_utilisateur');
+define('MYSQL_PASSWORD', 'mot_de_passe');
+?>
